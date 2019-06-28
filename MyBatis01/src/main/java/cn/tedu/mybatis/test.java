2c4bafa3021e2ba8f8ac8013f7ff3d13e2ead17b
zhu <1037256064@qq.com>
2019-06-28 22:41:24 +0800

    zhu1

--- MyBatis01/src/main/java/cn/tedu/mybatis/test.java
+++ MyBatis01/src/main/java/cn/tedu/mybatis/test.java
@@ -4,10 +4,14 @@ public class test {
 	private int age;
 
 	public int getAge() {
+		System.out.println("asdf");
+		System.out.println("asdf");
 		return age;
+		
 	}
 
 	public void setAge(int age) {
 		this.age = age;
 	}
+	
 }
